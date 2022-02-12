ba6811ca83b7e0c9d1444eb1546968e58ebe4fbc
tsukasa <taketsun.mg@gmail.com>
2022-02-12 15:35:03 +0900

    add directory

---
+++ interface/src/StuSample7.java
@@ -0,0 +1,9 @@
+public class StuSample7 {
+  public static void main(String[] args) throws Exception {
+    Student7 stu = new Student7("Mike");
+    stu.displayEng();
+
+    Baseball bb = new Baseball();
+    bb.displayEng();
+  }
+}
