1bfdb7f6ece169f41b7225edfbd97e51f4ae4ef1
tsukasa <taketsun.mg@gmail.com>
2022-02-12 16:04:38 +0900

    add directory

---
+++ package/src/StuSample8.java
@@ -0,0 +1,8 @@
+import pack.Student8;
+
+public class StuSample8 {
+  public static void main(String[] args) throws Exception {
+    Student8 stu = new Student8("菅原");
+    stu.display();
+  }
+}
---
+++ package/src/pack/Student8.java
@@ -0,0 +1,13 @@
+package pack;
+
+public class Student8 {
+  private String name;
+
+  public Student8(String name) {
+    this.name = name;
+  }
+
+  public void display() {
+    System.out.println("名前：" + name);
+  }
+}
