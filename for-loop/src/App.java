55a4c82ae157d74e827201fd8d34e63fa93ba3d8
tsukasa <taketsun.mg@gmail.com>
2022-02-15 13:47:08 +0900

    add directory

---
+++ for-loop/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+  public static void main(String[] args) throws Exception {
+    for(int i = 10; i >= 0; i--) {
+      System.out.println(i);
+    }
+
+    System.out.println("Happy new year!");
+  }
+}
