bf2404f7321601d6792bd8f1973952cd61fa066c
tsukasa <taketsun.mg@gmail.com>
2022-02-15 15:12:28 +0900

    add directory

---
+++ wrapper-classes/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+  public static void main(String[] args) throws Exception {
+    
+    Boolean a = true;
+    Character b = '@';
+    Integer c = 123;
+    Double d = 3.14;
+    String e = "Bro";
+
+    if(a == true) {
+      System.out.println("This is true");
+    }
+  }
+}
