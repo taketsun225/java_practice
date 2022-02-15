3ad40f9722bee5f43219de9a680059ef5c5c27bc
tsukasa <taketsun.mg@gmail.com>
2022-02-15 11:45:51 +0900

    add directory

---
+++ switches/src/App.java
@@ -0,0 +1,24 @@
+public class App {
+  public static void main(String[] args) throws Exception {
+    
+    String day = "pizza";
+
+    switch(day) {
+      case "Sunday": System.out.println("It is Sunday!");
+      break;
+      case "Monday": System.out.println("It is Monday!");
+      break;
+      case "Tuesday": System.out.println("It is Tuesday!");
+      break;
+      case "Wednesday": System.out.println("It is Wednesday!");
+      break;
+      case "Thursday": System.out.println("It is Thursday!");
+      break;
+      case "Friday": System.out.println("It is Friday!");
+      break;
+      case "Saturday": System.out.println("It is Saturday!");
+      break;
+      default: System.out.println("That is not day!");
+    }
+  }
+}
