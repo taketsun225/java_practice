361ebbb904f16491813b9bcec9648811540e85e1
tsukasa <taketsun.mg@gmail.com>
2022-02-15 11:34:29 +0900

    add directory

---
+++ if-statements/src/App.java
@@ -0,0 +1,16 @@
+public class App {
+  public static void main(String[] args) throws Exception {
+
+    int age = 15;
+
+    if (age >= 75) {
+      System.out.println("OK Boomer!");
+    } else if (age >= 18) {
+      System.out.println("You are an adult!");
+    } else if (age >= 13) {
+      System.out.println("You are a teenager!");
+    } else {
+      System.out.println("You are not an adult!");
+    }
+  }
+}
