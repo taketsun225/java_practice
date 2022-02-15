e98798182760a240f5982aeda05e634208745fc5
tsukasa <taketsun.mg@gmail.com>
2022-02-15 10:04:21 +0900

    add directory

---
+++ expressions/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+  public static void main(String[] args) throws Exception {
+    double friends = 10;
+    // friends++;
+    friends = (double)friends / 3;
+
+    System.out.println(friends);
+  }
+}
