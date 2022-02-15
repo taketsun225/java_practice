98aabcdb28b7c21b38b6b3b2d3feaaf95e1b2266
tsukasa <taketsun.mg@gmail.com>
2022-02-15 11:17:25 +0900

    add directory

---
+++ Rd-numbers/src/App.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+public class App {
+  public static void main(String[] args) throws Exception {
+    
+    Random random = new Random();
+
+    // int x = random.nextInt(6) + 1;
+    // double y = random.nextDouble();
+    boolean z = random.nextBoolean();
+
+    System.out.println(z);
+  }
+}
