3915d0243fc28c4d6acc467babd1466844575700
tsukasa <taketsun.mg@gmail.com>
2022-02-16 13:38:54 +0900

    add directory

---
+++ access-modifiers/src/package1/A.java
@@ -0,0 +1,10 @@
+package package1;
+import package2.*;
+
+public class A {
+  public static void main(String[] args) throws Exception {
+    
+    B b = new B();
+    System.out.println(b.privateMessage);
+  }
+}
---
+++ access-modifiers/src/package2/Asub.java
@@ -0,0 +1,11 @@
+package package2;
+import package1.*;
+
+public class Asub extends A {
+  
+  // public static void main(String[] args) throws Exception {
+    
+  //   Asub asub = new Asub();
+  //   System.out.println(asub.protectedMessage);
+  // }
+}
