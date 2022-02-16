40218ddd5636e3e049e958a3a0921e095dfc6212
tsukasa <taketsun.mg@gmail.com>
2022-02-16 11:22:36 +0900

    add directory

---
+++ sta-keyword/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+  public static void main(String[] args) throws Exception {
+
+    Friend friend1 = new Friend("Sponegbob");
+    Friend friend2 = new Friend("Patrick");
+    Friend friend3 = new Friend("Squidward");
+    Friend friend4 = new Friend("Sandy");
+
+    Friend.displayFriends();
+    // System.out.println(Friend.numberOfFriends);
+  }
+}
