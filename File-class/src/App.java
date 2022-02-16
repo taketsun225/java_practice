abf79d0c7b204210434af237e8402eca10268651
tsukasa <taketsun.mg@gmail.com>
2022-02-16 15:58:02 +0900

    add directory

---
+++ File-class/src/App.java
@@ -0,0 +1,18 @@
+import java.io.File;
+
+public class App {
+  public static void main(String[] args) throws Exception {
+
+    File file = new File("secret_message.txt");
+
+    if(file.exists()) {
+      System.out.println("That file exists! :O!");
+      System.out.println(file.getPath());
+      System.out.println(file.getAbsolutePath());
+      System.out.println(file.isFile());
+      file.delete();
+    } else {
+      System.out.println("That file doesn't exist :(");
+    }
+  }
+}
