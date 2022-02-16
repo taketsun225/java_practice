d414ef65ec3eb6ab5dcac1b1e7ae6734102ba094
tsukasa <taketsun.mg@gmail.com>
2022-02-16 09:14:14 +0900

    add directory

---
+++ objects/src/App.java
@@ -0,0 +1,16 @@
+public class App {
+  public static void main(String[] args) throws Exception {
+    
+    Car myCar1 = new Car();
+    Car myCar2 = new Car();
+
+    System.out.println(myCar1.make);
+    System.out.println(myCar1.model);
+    System.out.println();
+    System.out.println(myCar2.make);
+    System.out.println(myCar2.model);
+    System.out.println();
+    myCar1.drive();
+    myCar1.brake();
+  }
+}
