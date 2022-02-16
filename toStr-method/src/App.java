c959c8732b1ae3bfe36fda2f9e986c794fa19e69
tsukasa <taketsun.mg@gmail.com>
2022-02-16 10:20:53 +0900

    add directory

---
+++ toStr-method/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+  public static void main(String[] args) throws Exception {
+
+    Car car = new Car();
+
+    System.out.println(car.toString());
+    System.out.println(car);
+
+    // System.out.println(car.make);
+    // System.out.println(car.model);
+    // System.out.println(car.color);
+    // System.out.println(car.year);
+  }
+}
