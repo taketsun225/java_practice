6d9b6d31d5e1054b9095477011f2f03ce6b4fcd2
tsukasa <taketsun.mg@gmail.com>
2022-02-17 11:19:16 +0900

    add directory

---
+++ panels/src/App.java
@@ -0,0 +1,45 @@
+import java.awt.Color;
+import java.awt.BorderLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class App {
+  public static void main(String[] args) throws Exception {
+
+    ImageIcon icon = new ImageIcon("thumbsup32.png");
+
+    JLabel label = new JLabel();
+    label.setText("Hi");
+    label.setIcon(icon);
+    // label.setVerticalAlignment(JLabel.BOTTOM);
+    // label.setHorizontalAlignment(JLabel.RIGHT);
+    label.setBounds(100, 100, 75, 75);
+
+    JPanel redPanel = new JPanel();
+    redPanel.setBackground(Color.red);
+    redPanel.setBounds(0, 0, 250, 250);
+    redPanel.setLayout(null);
+
+    JPanel bluePanel = new JPanel();
+    bluePanel.setBackground(Color.blue);
+    bluePanel.setBounds(250, 0, 250, 250);
+    bluePanel.setLayout(null);
+
+    JPanel greenPanel = new JPanel();
+    greenPanel.setBackground(Color.green);
+    greenPanel.setBounds(0, 250, 500, 250);
+    greenPanel.setLayout(null);
+
+    JFrame frame = new JFrame();
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setLayout(null);
+    frame.setSize(750, 750);
+    frame.setVisible(true);
+    greenPanel.add(label);
+    frame.add(redPanel);
+    frame.add(bluePanel);
+    frame.add(greenPanel);
+  }
+}
